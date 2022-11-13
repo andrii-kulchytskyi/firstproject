0b9f4f4416a6a888400da323a04b3a02eae0079c
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2022-11-13 22:32:08 +0400

    props2

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,7 @@ function hello() {
 
 function App() {
     console.log("App is rendering");
-    return (
-        <div>
-            This is App component
 
-            <Accordion title={"Menu"} collapsed = {true}/>
-            <Accordion title={"Users"} collapsed = {false}/>
-            <R value={0}/>
-            <R value={1}/>
-            <R value={2}/>
-            <R value={3}/>
-            <R value={4}/>
-            <R value={5}/>
-        </div>
-    );
 }
 
 export default App;
