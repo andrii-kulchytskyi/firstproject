4e261dfc749030412adb590740acfada7780955c
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-03-07 00:56:32 +0400

    17(1/2)

--- src/components/React.memo.stories.tsx
+++ src/components/React.memo.stories.tsx
@@ -2,14 +2,17 @@ export default {
     title: 'React.memo.demo',
 }
 
-const Counter = (props: any) => {
+const NewMessagesCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
-const Users = (props: {users: string[]}) => {
-    return <div>{props.users.map(u=>u)}</div>
+const Users = (props: { users: string[] }) => {
+    return <div>{props.users.map(u => <div>{u}</div>)}</div>
 }
 
 
 export const Example1 = () => {
-
+    return <>
+        <NewMessagesCounter count={10}/>
+        <Users users={["Andrei", "Bogdan", "Valera"]}/>
+    </>
 }
